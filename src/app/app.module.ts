eb7d1080b3896765e6162666cdc5dd8b82209e73
Gabriel G. Martins <gabrielgmartinsbr@gmail.com>
2021-02-21 06:22:10 -0300

    add hammer scroll fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+declare var Hammer: any;
+
+export class MyHammerConfig extends HammerGestureConfig {
+  buildHammer(element: HTMLElement) {
+    const mc = new Hammer(element, {
+      touchAction: 'pan-y'
+    });
+    return mc;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -13,7 +24,14 @@ import { AppComponent } from './app.component';
     HammerModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      // hammer instantion with custom config
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
